b8c2931bfef2252c3a9ce582970b60489ebb66fe
Christoph Gockel <github@christophgockel.de>
2017-11-12 17:11:23 +0000

    Adds Edge support

--- changelog.md
+++ changelog.md
@@ -6,6 +6,9 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/) a
 
 
 ## [Unreleased]
+### Added
+- Support for Microsoft Edge
+
 ### Changes
 - No permissions are requested anymore
 
--- nein.js
+++ nein.js
@@ -1,16 +1,29 @@
-chrome.tabs.onCreated.addListener(function(tab) {
-  chrome.windows.get(tab.windowId, { populate: true }, function(currentWindow) {
-    if (currentWindow.tabs.length > 9) {
-      chrome.windows.get(tab.windowId, function(currentWindow) {
-        var createOptions = {
-          tabId: tab.id,
-          incognito: tab.incognito,
-          state: currentWindow.state
-        };
+var browser = (function() {
+  return window.browser || window.chrome;
+})();
 
-        chrome.windows.create(createOptions);
+browser.tabs.onCreated.addListener(function(tab) {
+  browser.windows.get(tab.windowId, { populate: true }, function(currentWindow) {
+    if (currentWindow.tabs.length > 9) {
+      browser.windows.get(tab.windowId, function(currentWindow) {
+        browser.windows.create(optionsForNewWindow(tab, currentWindow));
       });
     }
   });
 });
+
+function optionsForNewWindow(tab, oldWindow) {
+  var options = {
+    tabId: tab.id,
+    state: oldWindow.state
+  };
+
+  if (tab.hasOwnProperty("incognito")) {
+    options.incognito = tab.incognito;
+  } else {
+    options.inPrivate = tab.inPrivate;
+  }
+
+  return options;
+}
 
